2238d464529fe517081dfd71fa2c69d762482cb8
Johan Öhlin <johanohlin@gmx.com>
2022-12-01 19:08:56 +0100

    Day 1
    Done!

---
+++ 2022/1/1.py
@@ -0,0 +1,23 @@
+input_file = 'data1.txt'
+# input_file = 'testdata1.txt'
+with open(input_file, 'r') as f:
+    data = f.readlines()
+
+all_sums = []
+
+calorie_sum = 0
+n = 0
+save = n
+for line in data:
+    # Remove newline
+    line = line.strip()
+    if line:
+        calorie_sum += int(line)
+    else:
+        all_sums.append(calorie_sum)
+        calorie_sum = 0
+
+all_sums.append(calorie_sum)
+
+print('Part one:', max(all_sums))
+print('Part two:', sum(sorted(all_sums, reverse=True)[0:3]))

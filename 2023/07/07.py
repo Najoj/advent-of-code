82d144ceb5509b75d046f0ea5c5fc80bbe3a4948
Johan Öhlin <johanohlin@gmx.com>
2023-12-11 07:04:06 +0100

    Day 7 part 2 started

--- 2023/07/07.py
+++ 2023/07/07.py
@@ -8,7 +8,8 @@ with open(input_file, 'r') as f:
 
 class Hand:
     _card_order = {'A': 12, 'K': 11, 'Q': 10,
-                   'J': 9, 'T': 8, '9': 7,
+                   'J': 9,
+                   'T': 8, '9': 7,
                    '8': 6, '7': 5, '6': 4,
                    '5': 3, '4': 2, '3': 1,
                    '2': 0}
@@ -20,7 +21,11 @@ class Hand:
     ONE_PAIR = 1
     HIGH_CARD = 0
 
-    def __init__(self, _line: str):
+    def __init__(self, _line: str, part2=False):
+        self.part2 = part2
+        if part2:
+            self._card_order['J'] = -200
+
         self.hand = _line.split(' ')[0]
         self.bid = int(_line.split(' ')[1])
         self.rank = None
@@ -34,22 +39,35 @@ class Hand:
                 return self._card_order[oh] < self._card_order[sh]
 
     def get_type(self):
-        ls = len(set(self.hand))
+        if self.part2 and 'J' in self.hand:
+            commonest = ('', 0)
+            for l in [x for x in self.hand if x != 'J']:
+                _count = self.hand.count(l)
+                if commonest[1] < _count:
+                    commonest = (l, _count)
+                elif (commonest[1] == _count
+                      and self._card_order[commonest[0]] < self._card_order[l]):
+                    commonest = (l, _count)
+            _hand = self.hand.replace('J', commonest[0])
+        else:
+            _hand = self.hand
+
+        ls = len(set(_hand))
         if ls == 1:
             # XXXXX
             return self.FIVE_OF_A_KIND
 
         if ls == 2:
             # XXXXY
-            if (self.hand.count(self.hand[0]) == 1
-                    or self.hand.count(self.hand[0]) == 4):
+            if (_hand.count(_hand[0]) == 1
+                    or _hand.count(_hand[0]) == 4):
                 return self.FOUR_OF_A_KIND
             # XXXYY
             return self.FULL_HOUSE
 
         if ls == 3:
             # XXXYZ:
-            if re.match(r'^.*(.).*\1.*\1.*$', self.hand):
+            if re.match(r'^.*(.).*\1.*\1.*$', _hand):
                 return self.THREE_OF_A_KIND
             return self.TWO_PAIR
 
@@ -73,3 +91,16 @@ for hb in hand_bids:
     answer += rank * hb.bid
 
 print('Part 1:', answer)
+
+hand_bids = []
+for line in data:
+    hand_bids.append(Hand(line, True))
+
+hand_bids = sorted(hand_bids)
+rank = 0
+answer = 0
+for hb in hand_bids:
+    rank += 1
+    answer += rank * hb.bid
+
+print('Part 2:', answer)

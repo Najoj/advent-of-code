6c5145baa90f09bcac671eae1cbbedc510afb699
Johan Öhlin <johanohlin@gmx.com>
2025-12-02 05:37:39 +0100

    day 8 2023
    some old stuff

--- 2023/08/08.py
+++ 2023/08/08.py
@@ -1,8 +1,9 @@
 import copy
 import re
+import sys
 
 input_file = 'my_input.txt'
-#input_file = 'test_input2.txt'
+# input_file = 'test_input2.txt'
 with open(input_file, 'r') as f:
     data = f.readlines()
 
@@ -21,21 +22,21 @@ class Map:
         assert name not in self.nodes
         self.nodes[name] = left_right
 
-    def walk(self, left_right):
+    def walk(self, left_right, part2=False):
         assert left_right in DIRECTIONS
         self.current_node = self.nodes[self.current_node][left_right]
-        return self.current_node == 'ZZZ'
+        return self.current_node[-1] == 'Z' if part2 else self.current_node == 'ZZZ'
 
     def __repr__(self):
         return self.current_node
 
 
 walk = data[0].strip()
-map = Map()
+map_ = Map()
 for node in data[2:]:
     n, d = node.split('=')
     d = tuple(x.strip() for x in re.findall(r'(\w+)', d))
-    map.insert_node(n, d)
+    map_.insert_node(n, d)
 
 steps_until_zzz = 0
 reached_zzz = False
@@ -44,9 +45,9 @@ new_walk = copy.copy(walk)
 while not reached_zzz:
     w = new_walk.pop()
     if w == 'L':
-        reached_zzz = map.walk(LEFT)
+        reached_zzz = map_.walk(LEFT)
     elif w == 'R':
-        reached_zzz = map.walk(RIGHT)
+        reached_zzz = map_.walk(RIGHT)
     else:
         assert False
 
@@ -57,11 +58,51 @@ while not reached_zzz:
 
 print('Part 1:', steps_until_zzz)
 
+"""
+input_file = 'test_input3.txt'
+with open(input_file, 'r') as f:
+    data = f.readlines()
+"""
 
-walk = data[0].strip()
-queue = []
-map = Map()
+# Template map
+map_ = Map()
 for node in data[2:]:
     n, d = node.split('=')
     d = tuple(x.strip() for x in re.findall(r'(\w+)', d))
-    map.insert_node(n, d)
+    map_.insert_node(n, d)
+
+walk = data[0].strip()
+starting_nodes = [node for node in map_.nodes if node[-1] == 'A']
+queue = []
+for i in range(len(starting_nodes)):
+    n_map = copy.deepcopy(map_)
+    n_map.current_node = starting_nodes[i]
+    queue.append(n_map)
+
+reached_zzz = [False] * len(queue)
+all_reached = False
+walk = list(reversed(walk))
+new_walk = copy.copy(walk)
+steps_until_zzz = 0
+while not all(reached_zzz):
+    w = new_walk.pop()
+
+    i = 0
+
+    for m in queue:
+        if w == 'L':
+            reached_zzz[i] = m.walk(LEFT, True)
+        elif w == 'R':
+            reached_zzz[i] = m.walk(RIGHT, True)
+        else:
+            assert False
+        i += 1
+
+    steps_until_zzz += 1
+
+    if not new_walk:
+        new_walk += walk
+        sys.stdout.flush()
+
+print('Part 2:', steps_until_zzz)
+

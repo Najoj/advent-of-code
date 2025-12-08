e764ecce486314748090aac707765f89557ad1c3
Johan Öhlin <johanohlin@gmx.com>
2025-12-08 18:55:51 +0100

    day 5 done

--- 2025/5/2.py
+++ 2025/5/2.py
@@ -1,4 +1,23 @@
-import copy
+class Span:
+    def __init__(self, start, end):
+        assert start <= end
+        self.start = start
+        self.end = end
+    
+    def __repr__(self):
+        return f'Span({self.start}, {self.end})'
+    
+    def __len__(self):
+        # +1 to be inclusive, eg 5-10 would be a span of 6
+        return self.end - self.start + 1
+    
+    def __eq__(self, other):
+        return self.start == other.start and self.end == other.end
+
+    def __lt__(self, other):
+        return self.start <= self.end < other.start <= other.end
+    
+
 
 INPUT = 'example.in'
 INPUT = 'my.in'
@@ -7,88 +26,49 @@ INPUT = 'my.in'
 with open(INPUT) as file:
     lines = file.readlines()
 
-ranges = []
+spans = []
 fresh = 0
-min_max = [0, 0]
-mm_set = False
 for line in lines:
     line = line.strip()
-    if not line:
-        # empty line
-        continue
-    elif '-' in line:
+    if '-' in line:
         # a range
         f, t = line.split('-')
         f = int(f)
         t = int(t)
-        ranges.append((f, t+1))
-
-    else:
-        # an ingredient, dnc
-        continue
+        span = Span(f, t)
+        if span not in spans:
+            spans.append(span)
     # end if
 # end for
 
+def update_spans(_spans):
+    updated_spans = _spans
+    updated = False
+    for outer in _spans:
+        for inner in _spans:
+            if outer == inner:
+                continue
+            elif outer < inner or inner < outer:
+                continue
+            else:
+                # Merge
+                span = Span(min(outer.start, inner.start), max(outer.end, inner.end))
+                updated_spans.append(span)
+                try:
+                    updated_spans.remove(outer)
+                except ValueError:
+                    pass
+                try:
+                    updated_spans.remove(inner)
+                except ValueError:
+                    pass
+                    
+                updated = True
+    return updated_spans, updated
 
-def check_range(outer, inner):
-    updated = True
-    new_ranges = inner
-    if outer == inner or outer[1] < inner[0] or inner[1] < outer[0]:
-        # no overlap
-        updated = False
-    elif inner[0] <= outer[0] <= outer[1] <= inner[1]:
-        # inner [        ]
-        # outer   [   ]
-        new_ranges = (inner[0], inner[1])
-
-    elif outer[0] <= inner[0] <= outer[1] <= inner[1]:
-        # inner     [   ]
-        # outer   [   ]
-        new_ranges = (outer[0], inner[1])
-
-    elif inner[0] <= outer[0] <= inner[1] <= outer[1]:
-        # inner [    ]
-        # outer   [     ]
-        new_ranges = (inner[0], outer[1])
-
-    elif outer[0] <= inner[0] <= inner[1] <= outer[1]:
-        # inner   [   ]
-        # outer [        ]
-        new_ranges = (outer[0], outer[1])
-
-    else:
-        print(inner)
-        print(outer)
-
-    return new_ranges, updated
-
-def update_ranges(ranges):
-    """
-    >>> update_ranges([(1,2)])
-    [(1,2)]
-    """
-    new_ranges = copy.copy(ranges)
-    for outer in ranges:
-        for inner in ranges:
-            new_range, updated = check_range(outer, inner)
-            if updated:
-                new_ranges.remove(outer)
-                new_ranges.remove(inner)
-                new_ranges.append(new_range)
-                return new_ranges
-    return new_ranges
-
-while True:
-    new_ranges = update_ranges(ranges)
-    if ranges == new_ranges:
-        break
-    ranges = new_ranges
-    print(len(ranges))
-
-no_fresh = 0
-for f, t in ranges:
-    no_fresh += t-f
-
-if 'example.in' in INPUT:
-    assert no_fresh == 14, f'Got {no_fresh} != 14'
-print(no_fresh)
+updates = True
+while updates:
+    spans, updates = update_spans(spans)
+    
+sum_ = sum(len(s) for s in spans)
+print(sum_)
